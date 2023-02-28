d3732b04e6d93662bbca2c9d99fe290fe115536c
JepCast <ycastillop2@miumg.edu.gt>
2023-02-27 22:17:48 -0600

    Agregue la factorización y su resultado

--- Primer Parcial/Program.cs
+++ Primer Parcial/Program.cs
@@ -1,1 +1,10 @@
 ﻿// aquí comienza todo -Inserta musica epica!!!
+int factorial, num;
+Console.WriteLine("Por favor inserte a continuación el numero que desea factorizar");
+num= Convert.ToInt32(Console.ReadLine());
+factorial = 1;
+for (int i = 1; i <= num; i++)
+{
+    factorial *= i;
+}
+Console.WriteLine($"El factorial del {num} es: {factorial}");
