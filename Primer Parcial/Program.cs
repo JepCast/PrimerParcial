7317e9885a5f6be5fce6584d022c6a71fb0ad946
JepCast <ycastillop2@miumg.edu.gt>
2023-02-28 01:04:40 -0600

    Parte final con la secuencia de números junto con un try catch en caso de que el usuario cometa algún error.

--- Primer Parcial/Program.cs
+++ Primer Parcial/Program.cs
@@ -1,70 +1,84 @@
 ﻿// aquí comienza todo -Inserta musica epica!!!
-int factorial, num, nx3, numw1, nx5, numw2;
+int factorial, num, nx3, numw1, nx5, numw2, secuencia;
 
-Console.WriteLine("Hola, bienvenido, por favor inserte a continuación el numero que desea factorizar:");
-    num= Convert.ToInt32(Console.ReadLine());
+try
+{
+    Console.WriteLine("Hola, bienvenido, por favor inserte a continuación el numero que desea factorizar:");
+    num = Convert.ToInt32(Console.ReadLine());
     factorial = 1;
 
-Console.WriteLine("\nAhora por favor inserta un numero entero positivo que desees dividir entre 3: ");
-nx3 = Convert.ToInt32(Console.ReadLine());
-numw1 = 1;
-Console.Write("Los numeros enteros positivos menores o iguales divisibles entre 3 son: ");
-while (numw1 <= nx3)
-{
-    if (numw1 % 3 == 0)
+    Console.WriteLine("\nAhora por favor inserta un numero entero positivo que desees dividir entre 3: ");
+    nx3 = Convert.ToInt32(Console.ReadLine());
+    numw1 = 1;
+    Console.Write("Los numeros enteros positivos menores o iguales divisibles entre 3 son: ");
+    while (numw1 <= nx3)
     {
-        
-        Console.Write(numw1 + " ");
-    }
-    numw1++;
-}
+        if (numw1 % 3 == 0)
+        {
 
-Console.WriteLine("\nAhora por favor inserta un numero entero positivo que desees dividir entre 5: ");
-nx5 = Convert.ToInt32(Console.ReadLine());
-numw2 = 1;
-Console.Write("Los numeros enteros positivos menores o iguales divisibles entre 5 son: ");
-while (numw2 <= nx5)
-{
-    if (numw2 % 5 == 0)
+            Console.Write(numw1 + " ");
+        }
+        numw1++;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("\nAhora por favor inserta un numero entero positivo que desees dividir entre 5: ");
+    nx5 = Convert.ToInt32(Console.ReadLine());
+    numw2 = 1;
+    Console.Write("Los numeros enteros positivos menores o iguales divisibles entre 5 son: ");
+    while (numw2 <= nx5)
     {
+        if (numw2 % 5 == 0)
+        {
 
-        Console.Write(numw2 + " ");
+            Console.Write(numw2 + " ");
+        }
+        numw2++;
     }
-    numw2++;
-}
+
+    Console.WriteLine();
+    Console.WriteLine("\nAhora por favor inserta el numero que deseas para la secuencia de numeros: ");
+    secuencia = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"\nEl resultado de {secuencia} es:");
+    int s = 1;
+    Console.Write(" " + s);
+    do
+    {
+        s += 2;
+        Console.Write(" " + s);
+    }
+
+    while (s <= secuencia);
 
 
+    if (num == 0)
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
 
-
-
-
-if (num == 0 )
+    Console.Write("\nEl factorial de ");
     for (int i = 1; i <= num; i++)
     {
         factorial *= i;
     }
 
-Console.Write("\nEl factorial de ");
-for (int i = 1; i <= num; i++)
-    {
-        factorial *= i;
-    }
+    Console.Write(num);
+    int n = 1;
 
-Console.Write(num);
-int n = 1;
-    
-while (n < num)
+    while (n < num)
     {
-        Console.Write("X" + (num-n));
+        Console.Write("X" + (num - n));
         n++;
-    } 
-
-if (num == 0) 
-    { 
-        Console.Write(" es = 0"); 
     }
-else 
+
+    if (num == 0)
+    {
+        Console.Write(" es = 0");
+    }
+    else
     {
         Console.Write(" es = " + factorial);
     }
-
+} catch
+{ Console.WriteLine("Has insertado un dato no valido, por favor intentalo de nuevo!"); }
