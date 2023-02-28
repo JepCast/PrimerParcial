76d58dd8841870f2d77848e87235ae8f851b4a27
JepCast <ycastillop2@miumg.edu.gt>
2023-02-27 23:26:52 -0600

    Agregue para no solo obtener el resultado del factoria, si no también su NxNxNxN=N

--- Primer Parcial/Program.cs
+++ Primer Parcial/Program.cs
@@ -1,10 +1,35 @@
 ﻿// aquí comienza todo -Inserta musica epica!!!
 int factorial, num;
-Console.WriteLine("Por favor inserte a continuación el numero que desea factorizar");
-num= Convert.ToInt32(Console.ReadLine());
-factorial = 1;
+
+Console.WriteLine("Hola, bienvenido, favor inserte a continuación el numero que desea factorizar");
+    num= Convert.ToInt32(Console.ReadLine());
+    factorial = 1;
+
+if (num == 0 )
+    for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+
+Console.Write("\nEl factorial de ");
 for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+Console.Write(num);
+int n = 1;
+    
+while (n < num)
+    {
+        Console.Write("X" + (num-n));
+        n++;
+    } 
+
+if (num == 0) 
+    { 
+    Console.Write(" es = 0"); 
+}else 
 {
-    factorial *= i;
+    Console.Write(" es = " + factorial);
 }
-Console.WriteLine($"El factorial del {num} es: {factorial}");
+
